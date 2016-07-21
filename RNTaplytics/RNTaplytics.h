3b0cb44f0659423ad3fd4a0639eca5d17268c773
Aria Buckles <aria@magoosh.com>
2016-07-21 16:59:33 -0700

    first start

---
+++ RNTaplytics/RNTaplytics.h
@@ -0,0 +1,13 @@
+//
+//  RNTaplytics.h
+//  RNTaplytics
+//
+//  Created by Aria on 7/21/16.
+//  Copyright © 2016 Magoosh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNTaplytics : NSObject
+
+@end
---
+++ RNTaplytics/RNTaplytics.m
@@ -0,0 +1,13 @@
+//
+//  RNTaplytics.m
+//  RNTaplytics
+//
+//  Created by Aria on 7/21/16.
+//  Copyright © 2016 Magoosh. All rights reserved.
+//
+
+#import "RNTaplytics.h"
+
+@implementation RNTaplytics
+
+@end
